5d0f2fb9a5a3d9c6c325b3a479dcbe8801207be1
LeoKnox <LeoKnox@users.noreply.github.com>
2022-07-12 22:43:05 -0700

    Add files via upload

--- account/views.py
+++ account/views.py
@@ -27,6 +27,9 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.succes(request, 'Profile updated successfully')
+        else:
+            messages.error(request, 'Error updating your profile')
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
